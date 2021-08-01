db3e10511fe424594a3c6b3b2c52cc63dafac60a
Erin <60856806+erinisafox@users.noreply.github.com>
2021-08-01 15:17:04 -0500

    introducing frequency plots

--- plotter.py
+++ plotter.py
@@ -13,6 +13,16 @@ def normalci(array):
         stdev += (mean - X) ** 2
     return 1.96 * sqrt(stdev / (len(array) - 1)) / sqrt(len(array))
 
+# saves plots into a folder, given the plot and the axis labels and file name
+def savePlot(plt, xaxislabel, yaxislabel, file):
+    newdir = f"plot/{file.split('_')[0].split(os.sep)[-1]}/{file.split(os.sep)[-1][:-4]}"
+    if not os.path.exists(os.path.dirname(newdir)):
+        os.makedirs(os.path.dirname(newdir))
+    plt.savefig(f"{newdir}_{xaxislabel}_{yaxislabel}.png")
+
+    print(f"Saved {newdir}_{xaxislabel}_{yaxislabel}.png")
+    return
+
 
 # plot anything comparing two factors
 # acpl/movetime, acpl/timeleft, etc.
@@ -54,11 +64,29 @@ def compare(cpls, otherarray, a, b, c, xaxislabel, yaxislabel, file):
     plt.ylabel(yaxislabel)
     plt.title(file.split(os.sep)[-1])
 
-    newdir = f"plot/{file.split('_')[0].split(os.sep)[-1]}/{file.split(os.sep)[-1][:-4]}"
-    if not os.path.exists(os.path.dirname(newdir)):
-        os.makedirs(os.path.dirname(newdir))
-    plt.savefig(f"{newdir}_{xaxislabel}_{yaxislabel}.png")
+    return savePlot(plt, xaxislabel, yaxislabel, file)
 
-    return
+# plot any single array with frequency on the y-axis
+# cpl distribution, movetime distribution, etc.
+def frequency(array, a, b, c, xaxislabel, file):
+    plt.clf()
+
+    freq = [0] * ((b - a) // c + 1)
+    for X in array:
+        temp = int((X - a) // c)
+        if temp < 0:
+            temp = 0
+        if temp >= len(freq):
+            temp = len(freq)-1
+        freq[temp] += 1
+
+    xaxis = range(a, b + c, c)
+    plt.bar(xaxis, freq, width=c*0.75)
+    plt.ylim(bottom=0)
+    plt.xlabel(xaxislabel)
+    plt.ylabel("frequency")
+    plt.title(file.split(os.sep)[-1])
+
+    return savePlot(plt, xaxislabel, "frequency", file)
 
 # end of file
