f41f535e8fd6680230746bfdc24509c29fb78119
Erin <60856806+erinisafox@users.noreply.github.com>
2021-08-01 01:37:42 -0500

    added png saving to plot directory

--- plotter.py
+++ plotter.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import os
 from math import sqrt
 
 # 95% ci assuming gaussian, which is a bad assumption
@@ -18,6 +19,8 @@ def normalci(array):
 # groups cpls into buckets defidned by max bucket a and bin width b
 # then takes an average of those cpls per bucket to calculate acpls
 def compare(cpls, otherarray, a, b, c, xaxislabel, yaxislabel, file):
+    plt.clf()
+
     minmt = a
     maxmt = b  # seconds for overflow bucket
     spacing = c  # seconds for bin width
@@ -50,7 +53,11 @@ def compare(cpls, otherarray, a, b, c, xaxislabel, yaxislabel, file):
     plt.xlabel(xaxislabel)
     plt.ylabel(yaxislabel)
     plt.title(file.split("/")[-1])
-    plt.show()
+
+    newdir = f"plot/{file.split('_')[0].split(os.sep)[-1]}/{file.split(os.sep)[-1][:-4]}"
+    if not os.path.exists(os.path.dirname(newdir)):
+        os.makedirs(os.path.dirname(newdir))
+    plt.savefig(f"{newdir}_{xaxislabel}_{yaxislabel}.png")
 
     return
 
