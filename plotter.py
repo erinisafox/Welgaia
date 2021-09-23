a2ff17ca6f68689afff38bd389ddbbc9bc4b0b07
Erin <60856806+erinisafox@users.noreply.github.com>
2021-09-22 22:49:09 -0500

    code generalization and boxplots

--- plotter.py
+++ plotter.py
@@ -1,11 +1,8 @@
 import matplotlib.pyplot as plt
 import os
 from math import sqrt
-from math import exp
-from math import factorial
 
 # normal confidence intervals, 95%
-# abc method courtesy of Constantine Evans at caltech, is preferred, but ran into memory issues
 def normalci(array):
     if len(array) < 2:
         return 0
@@ -27,46 +24,52 @@ def savePlot(plt, xaxislabel, yaxislabel, file):
 
 # plot anything comparing two factors
 # acpl/movetime, acpl/timeleft, etc.
-# groups cpls into buckets defidned by max bucket a and bin width b
+# groups cpls into buckets defined by max bucket a and bin width b
 # then takes an average of those cpls per bucket to calculate acpls
-def compare(cpls, otherarray, a, b, c, xaxislabel, yaxislabel, file):
+def compare(yarray, xarray, a, b, c, xaxislabel, yaxislabel, boxyay, file):
     plt.clf()
 
     # clear out any nonetypes
-    cplstemp = []
-    otherarraytemp = []
-    for X in range(0, len(cpls)):
-        if cpls[X] is not None and otherarray[X] is not None:
-            cplstemp.append(cpls[X])
-            otherarraytemp.append(otherarray[X])
-    cpls = cplstemp
-    otherarray = otherarraytemp
+    yarraytemp = []
+    xarraytemp = []
+    for X in range(0, len(yarray)):
+        if yarray[X] is not None and xarray[X] is not None:
+            yarraytemp.append(yarray[X])
+            xarraytemp.append(xarray[X])
+    yarray = yarraytemp
+    xarray = xarraytemp
 
+    # various initializations
     minmt = a
     maxmt = b  # seconds for overflow bucket
     spacing = c  # seconds for bin width
-
     numbuckets = ((maxmt - minmt) // spacing + 1)
-    acpls = [[] for _ in range(numbuckets)]
+    yfreq = [[] for _ in range(numbuckets)]
     cis = [0] * numbuckets
-    for X in range(0, len(otherarray)):
-        temp = (int(otherarray[X]) - minmt) // spacing
+    xaxis = range(minmt, maxmt + spacing, spacing)
+
+    # sorts y datapoints for boxplots
+    for X in range(0, len(xarray)):
+        temp = (int(xarray[X]) - minmt) // spacing
         if temp > numbuckets - 1:
             temp = numbuckets - 1
         if temp < 0:
             temp = 0
-        acpls[temp].append(cpls[X])
+        yfreq[temp].append(yarray[X])
 
+    # calculates mean and 95% CI for errorbar overlay onto boxplot
+    yavg = yfreq[:]
     for X in range(0, len(cis)):
         # if there weren't any cpls in the bucket, assign arbitrary acpl
-        if len(acpls[X]) == 0:
-            acpls[X] = [-100]
-        cis[X] = normalci(acpls[X])
-        acpls[X] = sum(acpls[X]) / len(acpls[X])
+        if len(yavg[X]) == 0:
+            yavg[X] = [-100]
+        cis[X] = normalci(yavg[X])
+        yavg[X] = sum(yavg[X]) / len(yavg[X])
 
-    xaxis = range(minmt, maxmt + spacing, spacing)
-    plt.scatter(xaxis, acpls)
-    plt.errorbar(xaxis, acpls, yerr=cis, fmt="o")
+    # plotting business
+    if boxyay: # sometimes you don't want a boxplot because variables are discrete and synthetic
+        plt.boxplot(yfreq,showfliers=False,vert=True,positions=xaxis,widths=spacing/2)
+    plt.errorbar(xaxis, yavg, yerr=cis, fmt="^", color="green", elinewidth=3, markersize=7)
     plt.ylim(bottom=0)
     plt.xlabel(xaxislabel)
     plt.ylabel(yaxislabel)
