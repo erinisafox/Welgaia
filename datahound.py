dda74a5370c5a82008103ed2c8a90f40ae9192f2
Erin <60856806+erinisafox@users.noreply.github.com>
2021-09-22 22:49:35 -0500

    code generalization and a few new variables

--- datahound.py
+++ datahound.py
@@ -31,22 +31,14 @@ def calcPhase(fen):
 
 # run through every game and scavenge clock and eval data, if it exists
 def hound(file, focus, numgames):
-    singleordouble = 2 #to check whether to skip half the moves in a game or not
+    singleordouble = 2 # to check whether to skip half the moves in a game or not
     if focus == "":
         singleordouble = 1
 
     pgn = open(file)
 
-    maxcpl = 1000  # upper bound for cpl. +/-10 is what lichess likes to do, but creates weird effects at extremes.
-
-    acpllist = []
-    movetimelist = []
-    remainingtimelist = []
-    evalslist = []
-    legalmoveslist = []
-    phaseslist = []
-    isamistakelist = []
-    mistakespergamelist = []
+    maxcpl = 1000  # upper bound for cpl. +/-10 is what lichess likes to do, but creates weird effects at extremes
+    results = [ [] for _ in range(15) ] # 15 is arbitrary. increase if more space is needed
 
     # iterate through all the games
     for X in tqdm(range(0, numgames)):
@@ -76,9 +68,18 @@ def hound(file, focus, numgames):
             # initialize variables for white and black increment (in case of berserk)
             inctccolor = [inctc, inctc]
 
+            whitegameresult = game.headers["Result"]
+            if whitegameresult == "1-0":
+                whitegameresult = 1
+            elif whitegameresult == "0-1":
+                whitegameresult = 0
+            elif whitegameresult == "1/2-1/2":
+                whitegameresult = 0.5
+            else:
+                whitegameresult = None
+
             # get evals and clock times for both players
             # will only care about pov player later on
-
             board = game.board()
             board.reset()
             for node in game.mainline():
@@ -118,31 +119,66 @@ def hound(file, focus, numgames):
 
         # calculation of acpl, move time, etc. uses evals, time remaining, etc.
         # change the step size to 2 if you care about a specific player
+
+        # 0 = empty
+        # 1 = evaluations (player)
+        # 2 = remaining time (player)
+        # 3 = acpl (player)
+        # 4 = move times (player)
+        # 5 = mistake flag (player)
+        # 6 = number of legal moves (all)
+        # 7 = number of pieces left
+        # 8 = mistakes per game
+        # 9 = player rating (player)
+        # 10 = opponent rating
+        # 11 = game result (player)
         for X in range(start, len(timeleftlist), singleordouble):
             if evallist[X-1] is not None and evallist[X] is not None:
-                acpllist.append(calcCpl(evallist[X - 1], evallist[X], X))
-                if acpllist[-1] > 100:
-                    isamistakelist.append(100)
+                results[3].append(calcCpl(evallist[X - 1], evallist[X], X))
+                if results[3][-1] > 100:
+                    results[5].append(100)
                     mistakespergame += 1
                 else:
-                    isamistakelist.append(0)
+                    results[5].append(0)
             else:
-                acpllist.append(None)
-                isamistakelist.append(None)
+                results[3].append(None)
+                results[5].append(None)
             if timeleftlist[X-2] is not None and timeleftlist[X] is not None:
-                movetimelist.append(timeleftlist[X - 2] - timeleftlist[X] + inctccolor[X % 2])
-                remainingtimelist.append(timeleftlist[X])
+                results[4].append(timeleftlist[X - 2] - timeleftlist[X] + inctccolor[X % 2])
+                results[2].append(timeleftlist[X])
             else:
-                movetimelist.append(None)
-                remainingtimelist.append(None)
-            evalslist.append(evallist[X])
-            legalmoveslist.append(nummoveslist[X])
-            phaseslist.append(phaselist[X])
-        if len(evallist) > 1 and evallist[0] is not None:
-            mistakespergamelist.append(mistakespergame)
-        else:
-            mistakespergamelist.append(None)
+                results[4].append(None)
+                results[2].append(None)
+            if evallist[X] is not None:
+                results[1].append((-1)**(X%2) * evallist[X])
+            else:
+                results[1].append(evallist[X])
+            results[6].append(nummoveslist[X])
+            results[7].append(phaselist[X])
 
-    return evalslist, remainingtimelist, acpllist, movetimelist, isamistakelist, legalmoveslist, phaseslist, mistakespergamelist
+            if X % 2 == 0: # white is making the move
+                if whitegameresult is not None:
+                    results[11].append(whitegameresult)
+                else:
+                    results[11].append(None)
+                results[9].append(int(game.headers["WhiteElo"]))
+                results[10].append(int(game.headers["BlackElo"])-int(game.headers["WhiteElo"]))
+            else:
+                if whitegameresult is not None:
+                    if X%2 == 1:
+                        results[11].append(1-whitegameresult)
+                    else:
+                        results[11].append(whitegameresult)
+                else:
+                    results[11].append(None)
+                results[9].append(int(game.headers["BlackElo"]))
+                results[10].append(int(game.headers["WhiteElo"])-int(game.headers["BlackElo"]))
+
+        if len(evallist) > 1 and evallist[0] is not None:
+            results[8].append(mistakespergame)
+        else:
+            results[8].append(None)
+
+    return results
 
 # end of file
